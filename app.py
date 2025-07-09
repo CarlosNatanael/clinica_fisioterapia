4731635f96b64a5511f23765608167e08ae6dec6
CarlosNatanael <carlosnatha2345@gmail.com>
2025-07-09 20:17:35 -0300

    .

--- app.py
+++ app.py
@@ -521,6 +521,3 @@ def deletar_plano(id):
 @app.route('/suporte')
 def suporte():
     return render_template('suporte.html')
-
-if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=True)
